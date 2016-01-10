85dd528e49c98860529a4be7f725edbfcefc0205
Zohar Kelrich <lumimies@gmail.com>
2016-01-11 00:06:32 +0200

    Try some different types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod observer;
 #[test]
 fn it_works() {
-	
+	let s = observer::test_source::from_iter((1..5).into_iter());
+	let s = observer::filter::new(s, |&x| { x > 5});
 }
--- src/observer.rs
+++ src/observer.rs
@@ -1,60 +1,74 @@
 
 pub trait Observer<T> {
+    type Item;
 	fn on_next(self, value : T) -> Self;
 	fn on_completed(self);
 	// Not sure how the error stuff looks in Rust
 	// fn on_error(Self/*,???*/);
 }
 
-pub trait Observable<T, O : Observer<T>> {
+pub trait Subscribable<O : Observer<Self::Item>> : Observable {
     type Subscription : Drop;
-	fn subscribe(&self, o : O) -> Self::Subscription;
+	fn subscribe(self, o : O) -> Self::Subscription;
 }
 
-struct SubscriptionAdapter<Inner : Drop> { inner_subscription : Inner }
+pub trait Observable {
+    type Item;
+}
+
+struct SubscriptionAdapter<Inner : Drop> { _inner_subscription : Inner }
 impl<Inner : Drop> Drop for SubscriptionAdapter<Inner> {
     fn drop(&mut self) {}
 }
 impl<Inner: Drop> SubscriptionAdapter<Inner> {
     pub fn new(inner : Inner) -> Self {
-        SubscriptionAdapter { inner_subscription: inner }
+        SubscriptionAdapter { _inner_subscription: inner }
     }
 }
 pub mod filter {
     use std::marker::PhantomData;
-    use super::{Observer, Observable, SubscriptionAdapter};
+    use super::{Observer, Observable, Subscribable, SubscriptionAdapter};
     pub struct Filter<T, Inner, F : Fn(&T) -> bool> { f : F, inner : Inner, _t : PhantomData<T> }
-
+    impl<T, O, F: Fn(&T) ->bool> Observable for Filter<T,O,F> { type Item = T; }
     struct FilterObserver<T,O : Observer<T>, F : Fn(&T) -> bool> { f : F, o : O, _t : PhantomData<T> }
 
     impl<T, O : Observer<T>, F : Fn(&T) -> bool> Observer<T> for FilterObserver<T, O, F> {
-            fn on_next(self, value : T) -> Self {
-                if (self.f)(&value) {
-                    FilterObserver { f: self.f, o: self.o.on_next(value), _t: self._t }
-                } else { self }
-            }
-            fn on_completed(self) {
-                self.o.on_completed();
-            }
+        type Item = T;
+        fn on_next(self, value : T) -> Self {
+            if (self.f)(&value) {
+                FilterObserver { f: self.f, o: self.o.on_next(value), _t: self._t }
+            } else { self }
         }
+        fn on_completed(self) {
+            self.o.on_completed();
+        }
+    }
 
-    impl<T, Q : Observer<T>, O : Observable<T,FilterObserver<T, Q, F>>, F : Fn(&T)->bool + Clone> Observable<T, Q> for Filter<T,O,F> {
+    impl<T, Q : Observer<T, Item=T>, O : Observable<Item = T> + Subscribable<FilterObserver<T, Q, F>>, F : Fn(&T)->bool> Subscribable<Q> for Filter<T,O,F> {
         type Subscription = SubscriptionAdapter<O::Subscription>;
-        fn subscribe(&self, o : Q) -> Self::Subscription {
-            let observer = FilterObserver { f: self.f.clone(), o: o, _t: PhantomData };
+        fn subscribe(self, o : Q) -> Self::Subscription {
+            let observer = FilterObserver { f: self.f, o: o, _t: PhantomData };
             SubscriptionAdapter::<O::Subscription>::new(self.inner.subscribe(observer))
         }
+    }
+    pub fn new<O : Observable, F : Fn(&O::Item) -> bool>(seq : O, f : F) -> Filter<O::Item,O,F> {
+        
+        Filter { inner: seq, f: f, _t: PhantomData}
     }
     // add code here
 }
 
 pub mod map {
     use std::marker::PhantomData;
-    use super::{Observer, Observable, SubscriptionAdapter};
-    pub struct Map<T, S, Inner, F: Fn(T) -> S> { f : F, inner : Inner, _t:PhantomData<T>, _s:PhantomData<S> }
+    use super::{Observer, Observable, Subscribable, SubscriptionAdapter};
+    pub struct Map<T, Inner : Observable, F: Fn(Inner::Item) -> T> { f : F, inner : Inner, _t:PhantomData<T> }
+    impl<T, Inner : Observable, F: Fn(Inner::Item) -> T> Observable for Map<T,Inner,F> {
+        type Item = T;
+    }
     struct MapObserver<T, S, O : Observer<S>, F : Fn(T) -> S> { f : F, o : O, _t : PhantomData<T>, _s : PhantomData<S> }
 
     impl<T, S, O: Observer<S>, F : Fn(T) -> S> Observer<T> for MapObserver<T, S, O, F> {
+        type Item = T;
         fn on_next(self, value : T) -> Self {
             let value = (self.f)(value);
             MapObserver { f: self.f, o: self.o.on_next(value), _t: self._t, _s: self._s}
@@ -64,11 +78,41 @@ pub mod map {
         }
     }
 
-    impl<T, S, Q : Observer<S>, O : Observable<T, MapObserver<T, S, Q, F>>, F : Fn(T) -> S + Clone> Observable<S, Q> for Map<T, S, O, F> {
+    impl<T, Q : Observer<T, Item = T>, O : Observable<Item = T> + Subscribable<MapObserver<Q::Item, T, Q, F>>, F : Fn(O::Item) -> T> Subscribable<Q> for Map<T, O, F> {
         type Subscription = SubscriptionAdapter<O::Subscription>;
-        fn subscribe(&self, o : Q) -> Self::Subscription {
-            let observer = MapObserver { f: self.f.clone(), o : o, _t : PhantomData, _s : PhantomData };
+        fn subscribe(self, o : Q) -> Self::Subscription {
+            let observer = MapObserver { f: self.f, o: o, _t: PhantomData, _s: PhantomData };
             SubscriptionAdapter::<O::Subscription>::new(self.inner.subscribe(observer))
         }
     }
 }
+
+pub mod test_source {
+    use std::sync::{Arc,Weak};
+    use std::iter::Iterator;
+    use super::{Observer, Observable, Subscribable};
+    pub struct TestSequence<I : Iterator> { it : I }
+    impl<I : Iterator> Observable for TestSequence<I> { type Item = I::Item; }
+    struct Sub { _p : Arc<()> }
+    impl Drop for Sub { fn drop(&mut self) {} }
+    pub fn from_iter<I: Iterator, >(it : I) -> TestSequence<I> { TestSequence { it: it } }
+    impl<I : Iterator, Q : Observer<I::Item>> Subscribable<Q> for TestSequence<I> {
+        type Subscription = Sub;
+        fn subscribe(self, o : Q) -> Self::Subscription {
+            let mut o = o;
+            for x in self.it {
+                o = o.on_next(x);
+            }
+            o.on_completed();
+            Sub { _p: Arc::new(()) }
+        }
+    }
+
+}
+
+
+/*impl<T, Q: Observer<T>, O : Subscribable<T,Q>> O {
+	fn filter(self, predicate : Fn(&T) -> bool) -> Filter::Filter<T,Self> {
+		Filter { f: predicate, inner: self }
+	}
+}*/
